f2a91ea8d91b4fce3b38a5ac4ccbb0ed7399888b
renebaebaexy <108109142+renebaebaexy@users.noreply.github.com>
2022-06-24 16:01:19 +0800

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static async Task DoSomeWorkAsync()
+        {
+            Console.WriteLine("Inside DoSomeWorkAsync");
+            await Task.Delay(2000);
+            Console.WriteLine("End of DoSomeWorkAsync");
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Main Program");
+            Task task = DoSomeWorkAsync();
+            Console.WriteLine("End Main Program");
+            //await task;
+        }
+    }
+
+}
